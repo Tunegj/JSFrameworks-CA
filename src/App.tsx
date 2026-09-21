2cf7b7bec8d6f52e382cc4088139917703d46e93
Tone Gjerde <tongje04878@stud.noroff.no>
2026-09-21 10:43:09 +0200

    chore: setup project structure and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { useState } from "react";
-import "./App.css";
+import "./index.css";
+import { AppRoutes } from "./routes";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1>Online Store</h1>
-    </>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+export function HomePage() {
+  return (
+    <main>
+      <h1>Online Shop</h1>
+    </main>
+  );
+}
