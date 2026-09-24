375cb86dc612fcf095986d81399e009a8d0a97f3
Tone Gjerde <tongje04878@stud.noroff.no>
2026-09-24 12:26:32 +0200

    fix: add Netlify SPA redirect

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+
 export function ContactPage() {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
   return (
     <main>
       <h1>CONTACT</h1>
+      <form>
+        <label htmlFor="fullName">Full Name</label>
+        <input
+          type="text"
+          id="fullName"
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="subject">Subject</label>
+        <input
+          type="text"
+          id="subject"
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
+        />
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <button type="submit">Send</button>
+      </form>
     </main>
   );
 }
